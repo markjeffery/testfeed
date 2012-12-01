80383b889e7c539cda271de67ce0936d2ef4a77f
Mark Jeffery <markjeffery@Helens-MacBook-Air.local>
2012-12-01 21:51:48 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testfeed::Application.config.secret_token = '2b7b7a7dd3daf65f506819fa85b05c3d2981e4c984d55ef2afddd99eaab49d796a2539e5f667e4fca4167d430a014dec543d7607571edc51094b42bec51b2ac4'
